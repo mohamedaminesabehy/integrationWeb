d96e3a6872c8b051679557d460024c67e13d8dda
mohamedaminesabehy <110574315+mohamedaminesabehy@users.noreply.github.com>
2025-03-04 02:43:47 +0100

    F et H

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#8E1616",
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
+
+
+
